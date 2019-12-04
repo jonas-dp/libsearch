9e8c9d599c0583563647f0bbc3f94d3d617c4b03
Jonas De Pelsmaeker <jonasdepelsmaeker@gmail.com>
2019-12-04 21:27:19 +0100

    added favicon to html page

--- src/HTMLOutput.py
+++ src/HTMLOutput.py
@@ -38,6 +38,8 @@ class HTMLOutput(Singleton, object):
                 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
                     integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
+                <link rel="icon" href="../img/icon.ico" type="image/x-icon"/>
+
                 <title>LibSearch</title>
             </head>
 
