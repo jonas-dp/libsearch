dea1a0c4814b3df7e52dc16f49c5886d6ce6e4f9
Jonas De Pelsmaeker <jonasdepelsmaeker@gmail.com>
2020-07-05 11:29:50 +0200

    added correct MIME tag for .js files on server

--- src/WebServer.py
+++ src/WebServer.py
@@ -10,6 +10,9 @@ class WebServer(Singleton, object):
         web_dir = os.path.join(Path(os.path.dirname(__file__)).parent, 'www')
         os.chdir(web_dir)
 
-        server = HTTPServer(('localhost', 8080), SimpleHTTPRequestHandler)
+        req_handler = SimpleHTTPRequestHandler
+        req_handler.extensions_map['.js'] = 'application/x-javascript'
+
+        server = HTTPServer(('localhost', 8080), req_handler)
         server.serve_forever()
         print("tetteeeen")
